c9490dc75f4bbae5440de6310a44e09a18eeac63
ohnapaula <ohnapaula@gmail.com>
2024-06-26 11:12:55 -0300

    Aplicação de melhoria no nome das variáveis

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 //primeiro desafio de lógica do Felipão da DIO
-let heroi = "Ana da Selva de Pedra"
-let xp = -1
+let nomeHeroi = "Ana da Selva de Pedra"
+let quantidadeExperiencia = -1
 
-if (xp >= 0 && xp <= 1000) {
-    console.log("O Herói de nome " + heroi + " está no nível Ferro!")
-} else if (xp >= 1001 && xp <= 2000) {
-    console.log("O Herói de nome " + heroi + " está no nível Bronze!")
-} else if (xp >= 2001 && xp <= 5000) {
-    console.log("O Herói de nome " + heroi + " está no nível Prata!")
-} else if (xp >= 5001 && xp <= 7000) {
-    console.log("O Herói de nome " + heroi + " está no nível Ouro!")
-} else if (xp >= 7001 && xp <= 8000) {
-    console.log("O Herói de nome " + heroi + " está no nível Platina!")
-} else if (xp >= 8001 && xp <= 9000) {
-    console.log("O Herói de nome " + heroi + " está no nível Ascendente!")
-} else if (xp >= 9001 && xp <= 10000) {
-    console.log("O Herói de nome " + heroi + " está no nível Imortal!")
-} else if (xp >= 10001) {
-    console.log("O Herói de nome " + heroi + " está no nível Radiante!")
+if (quantidadeExperiencia >= 0 && quantidadeExperiencia <= 1000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ferro!")
+} else if (quantidadeExperiencia >= 1001 && quantidadeExperiencia <= 2000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Bronze!")
+} else if (quantidadeExperiencia >= 2001 && quantidadeExperiencia <= 5000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Prata!")
+} else if (quantidadeExperiencia >= 5001 && quantidadeExperiencia <= 7000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ouro!")
+} else if (quantidadeExperiencia >= 7001 && quantidadeExperiencia <= 8000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Platina!")
+} else if (quantidadeExperiencia >= 8001 && quantidadeExperiencia <= 9000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ascendente!")
+} else if (quantidadeExperiencia >= 9001 && quantidadeExperiencia <= 10000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Imortal!")
+} else if (quantidadeExperiencia >= 10001) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante!")
 } else {
     console.log("Erro: quantidade de XP inválida para classificação de Herói!")
 }
